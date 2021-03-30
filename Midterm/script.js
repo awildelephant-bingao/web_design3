6c0922dc0019a4087cdfa5c806b6e42b9791914a
awildelephant-bingao <65309437+awildelephant-bingao@users.noreply.github.com>
2021-03-30 11:24:19 -0700

    Home page new

--- Midterm/script.js
+++ Midterm/script.js
@@ -1,40 +1,33 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict'
-  
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Loop over them and prevent submission
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
 
 
-  const formPopup = document.querySelector(".form-popup");
+  const loginPopup = document.querySelector(".login-popup");
+  const close = document.querySelector(".close");
 
-  window.addEventListener("load", function(){
-    showPopup();
+
+  window.addEventListener("load",function(){
+ 
+   showPopup();
+   // setTimeout(function(){
+   //   loginPopup.classList.add("show");
+   // },5000)
+
   })
 
   function showPopup(){
-    const timeLimit = 5 //seconds;
-    let i=0;
-  const timer =setInterval(function(){
-    i++;
-    if(i==timeLimit){
-      clearInterval(timer);
-      formPopup.classList.add("show");
-    }
-    console.log(i)
-  },1000);
+        const timeLimit = 5 // seconds;
+        let i=0;
+        const timer = setInterval(function(){
+         i++;
+         if(i == timeLimit){
+          clearInterval(timer);
+          loginPopup.classList.add("show");
+         } 
+         console.log(i)
+        },1000);
   }
+
+
+  close.addEventListener("click",function(){
+    loginPopup.classList.remove("show");
+  })
+
