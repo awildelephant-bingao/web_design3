12d2e0d08b225be7f7827db22a87b15a8919c4a0
awildelephant-bingao <65309437+awildelephant-bingao@users.noreply.github.com>
2021-03-31 21:49:11 -0700

    form for team
    not working

--- Midterm/script.js
+++ Midterm/script.js
@@ -31,3 +31,26 @@
     loginPopup.classList.remove("show");
   })
 
+
+
+  
+(function () {
+  'use strict'
+
+
+  var forms = document.querySelectorAll('.needs-validation')
+
+
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
+
