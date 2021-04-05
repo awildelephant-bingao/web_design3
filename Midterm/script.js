b6a6a5efe0f8014a026c712a25b3f6b7e66c443f
awildelephant-bingao <65309437+awildelephant-bingao@users.noreply.github.com>
2021-04-04 22:37:45 -0700

    form and navigation

--- Midterm/script.js
+++ Midterm/script.js
@@ -33,13 +33,12 @@
 
 
 
-  
+
 (function () {
   'use strict'
 
-
+  
   var forms = document.querySelectorAll('.needs-validation')
-
 
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
@@ -53,4 +52,3 @@
       }, false)
     })
 })()
-
